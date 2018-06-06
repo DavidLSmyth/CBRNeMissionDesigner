bdf48e8e9a769dadf79f4ee994fd02812f409858
DavidLSmyth <d.smyth10@nuigalway.ie>
2018-06-06 11:08:11 +0100

    commit before refactoring GPSCoordinate classes to use WGS84 and UTM coordinates

--- src/GPSUtils/CoordinateInterface.java
+++ src/GPSUtils/CoordinateInterface.java
@@ -1,7 +1,10 @@
 package GPSUtils;
-
+import org.geotools.*;
 //An interface that any GPSCoordinate class must adhere to 
+import org.osgeo.proj4j.CoordinateReferenceSystem;
 public interface CoordinateInterface {
 
 	public double getMetresToOther(CoordinateInterface coord);
+	Coordin
+	
 }
